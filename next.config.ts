87eff36f7e13a2e0ba7a4ff75439a992478885be
Kholmurod <khairulloev.kh@gmail.com>
2025-04-26 16:19:52 +0500

    for deploying

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig: NextConfig = {
-  images:{
-    domains:["store-api.softclub.tj"]
-  }
+  images: {
+    domains: ["store-api.softclub.tj"],
+  },
+  reactStrictMode: true,  // Recommended for production
+  swcMinify: true         // Recommended for faster builds
 };
 
-const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
